e8965e8a4031e0599eb585c49b37b6fbea265b5e
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2016-12-17 14:58:13 +0300

    Initial Commit

---
+++ Instagram Clone with Firebase/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Instagram Clone with Firebase
+//
+//  Created by Atıl Samancıoğlu on 17/12/2016.
+//  Copyright © 2016 Atıl Samancıoğlu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
