15a7f8faca598668bd23ecb46c38fbe23124249d
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2016-12-18 17:34:15 +0300

    first commit

---
+++ Instagram Clone with Firebase/cell.swift
@@ -0,0 +1,24 @@
+//
+//  cell.swift
+//  Instagram Clone with Firebase
+//
+//  Created by Atıl Samancıoğlu on 17/12/2016.
+//  Copyright © 2016 Atıl Samancıoğlu. All rights reserved.
+//
+
+import UIKit
+
+class cell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
